1f85f09b36916a6be762ad7ac93e187d458b509f
Vlad <vladbaykin@gmail.com>
2021-05-19 22:58:38 +0300

    Преобразование JSON в POJO. JsonObject

---
+++ chapter_002/src/main/java/io/serialization/json/A.java
@@ -0,0 +1,19 @@
+package io.serialization.json;
+
+import org.json.JSONPropertyIgnore;
+
+public class A {
+    private B b;
+
+    @JSONPropertyIgnore
+    public B getB() {
+        return b;
+    }
+
+    public void setB(B b) {
+        this.b = b;
+    }
+    public String getHello() {
+        return "Hello";
+    }
+}
--- chapter_002/src/main/java/io/serialization/json/Dog.java
+++ chapter_002/src/main/java/io/serialization/json/Dog.java
@@ -1,15 +1,15 @@
 package io.serialization.json;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @XmlRootElement(name = "dog")
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -33,6 +33,22 @@ public class Dog {
         this.commands = commands;
     }
 
+    public boolean isSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Contact getContactOwn() {
+        return contactOwn;
+    }
+
+    public String[] getCommands() {
+        return commands;
+    }
+
     @Override
     public String toString() {
         return "Dog{"
@@ -44,22 +60,28 @@ public class Dog {
     }
 
     public static void main(String[] args) throws Exception {
+
+        /* JSONObject из json-строки строки */
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+        /* JSONArray из ArrayList */
+        List<String> list = new ArrayList<>();
+        list.add("give a paw");
+        list.add("go");
+        JSONArray jsonCommands = new JSONArray(list);
+
+        /* JSONObject напрямую методом put */
         final Dog dog = new Dog(true, 2, new Contact("+7983"), "sit", "vote");
-        JAXBContext context = JAXBContext.newInstance(Dog.class);
-        Marshaller marshaller = context.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", dog.isSex());
+        jsonObject.put("age", dog.getAge());
+        jsonObject.put("contactOwn", jsonContact);
+        jsonObject.put("commands", jsonCommands);
 
-        String xml;
-        try (StringWriter writer = new StringWriter()) {
-            marshaller.marshal(dog, writer);
-            xml = writer.getBuffer().toString();
-            System.out.println(xml);
-        }
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject);
 
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        try (StringReader reader = new StringReader(xml)) {
-            Dog result = (Dog) unmarshaller.unmarshal(reader);
-            System.out.println(result);
-        }
+        /* Преобразуем объект person в json-строку */
+        System.out.println(new JSONObject(dog));
     }
 }

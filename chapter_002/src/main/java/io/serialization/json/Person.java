f18dac8a96f679aa2da655ef83484ad51c92aeda
Vlad <vladbaykin@gmail.com>
2021-05-19 16:51:02 +0300

    JAXB. Преобразование XML в POJO.

--- chapter_002/src/main/java/io/serialization/json/Person.java
+++ chapter_002/src/main/java/io/serialization/json/Person.java
@@ -1,15 +1,28 @@
 package io.serialization.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Person {
-    private final boolean sex;
-    private final int age;
-    private final Contact contact;
-    private final String[] statuses;
+    @XmlAttribute
+    private boolean sex;
+
+    @XmlAttribute
+    private int age;
+    private Contact contact;
+    @XmlElementWrapper(name = "statuses")
+    @XmlElement(name = "status")
+    private String[] statuses;
+
+    public Person() {
+    }
 
     public Person(boolean sex, int age, Contact contact, String... statuses) {
         this.sex = sex;
@@ -28,26 +41,29 @@ public class Person {
                 + '}';
     }
 
-    public static void main(String[] args) {
-        final Person person = new Person(false, 30,  new Contact("11-111"), "Worker", "Married");
+    public static void main(String[] args) throws Exception {
+        final Person person = new Person(
+                false, 30,  new Contact("11-111"), "Worker", "Married");
 
-        /* Преобразуем объект person в json-строку. */
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(person));
-
-        /* Модифицируем json-строку */
-        final String personJson =
-                "{"
-                    + "\"sex\":false,"
-                    + "\"age\":35,"
-                    + "\"contact\":"
-                        + "{"
-                            + "\"phone\":\"+7(924)111-111-11-11\""
-                        + "},"
-                    + "\"statuses\":"
-                        + "[\"Student\",\"Free\"]"
-                    + "}";
-        final Person personMod = gson.fromJson(personJson, Person.class);
-        System.out.println(personMod);
+        // Получаем контекст для доступа к АПИ
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        // Создаем сериализатор
+        Marshaller marshaller = context.createMarshaller();
+        // Указываем, что нам нужно форматирование
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml;
+        try (StringWriter writer = new StringWriter()) {
+            // Сериализуем
+            marshaller.marshal(person, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        // Для десериализации нам нужно создать десериализатор
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            // десериализуем
+            Person result = (Person) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 }
